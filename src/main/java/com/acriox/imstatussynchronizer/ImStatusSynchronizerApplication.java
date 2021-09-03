f1a264e5a7d9515c2f46bb0761a5e48054edf6dd
Alex Cornea <cornea.alex@yandex.com>
2021-09-03 22:04:50 +0300

    Bootstrap project base using Spring Initializer

---
+++ src/main/java/com/acriox/imstatussynchronizer/ImStatusSynchronizerApplication.java
@@ -0,0 +1,12 @@
+package com.acriox.imstatussynchronizer;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ImStatusSynchronizerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ImStatusSynchronizerApplication.class, args);
+	}
+}
---
+++ src/test/java/com/acriox/imstatussynchronizer/ImStatusSynchronizerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.acriox.imstatussynchronizer;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ImStatusSynchronizerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
